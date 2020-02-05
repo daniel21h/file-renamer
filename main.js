72c7e9c5c72a5f1d77d4af2bd2e816d840706ddd
Your Name <you@example.com>
2020-02-05 20:55:42 -0300

    returning file

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+fs.readdir("./", (err, files) => {
+    console.log(files)
+})
+
+array.forEach(function(element, inex, array) {
+
+
+})
+

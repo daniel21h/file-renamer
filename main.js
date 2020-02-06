b6daee5c85451fbc1c056d66e30c4068de2bc52a
Your Name <you@example.com>
2020-02-05 21:22:15 -0300

    renaming and target working

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const fs = require('fs')
 
 const app = express()
 
-fs.readdir("./", (err, files) => {
+fs.readdir("./test", (err, files) => {
     console.log(files)
 })
 
-array.forEach(function(element, inex, array) {
-
+fs.rename('./test/Boletos.pdf', './teste/Perfeito.pdf', function(err) {
+    if ( err ) console.log('ERROR: ' + err)
 
 })
-
